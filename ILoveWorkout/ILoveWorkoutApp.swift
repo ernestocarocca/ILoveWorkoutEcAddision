3298bdd24a8c4b33491388ab1096e4c01bfcd898
JonasB91 <JonasB91@github.com>
2023-01-23 11:12:17 +0100

    Hej Första Commit

---
+++ ILoveWorkout/ILoveWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILoveWorkoutApp.swift
+//  ILoveWorkout
+//
+//  Created by Jonas Backas on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct ILoveWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
